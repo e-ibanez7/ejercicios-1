26e32c5db3b93a972dfba284ade3cb8fe65cbee5
e-ibanez7 <116891559+e-ibanez7@users.noreply.github.com>
2022-11-14 02:17:36 +0100

    Add files via upload

--- Fechas/src/main/java/com/curso/fechas/Fechas.java
+++ Fechas/src/main/java/com/curso/fechas/Fechas.java
@@ -5,13 +5,61 @@
 
 package com.curso.fechas;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author eibaneza
  */
 public class Fechas {
-
+    
+    public static int calculateDays (String date1, String date2){
+        String [] date1V = date1.split("/");
+        String [] date2V = date2.split("/");
+        int leapYears=0;
+        int totalDays=0;
+        
+        int [] parsedDate1 = new int [3];
+        int [] parsedDate2 = new int [3];
+        
+        for (int i=0;i<3;i++) {
+            parsedDate1[i] = Integer.parseInt(date1V[i]);
+            parsedDate2[i] = Integer.parseInt(date2V[i]);
+        }
+        if (parsedDate1[2] < parsedDate2[2]){
+            for (int i=parsedDate1[2]; i<=parsedDate2[2]; i++){
+                if (isLeapYear(i))
+                    leapYears++;
+            }
+            if (parsedDate1[2] - parsedDate2[2] != 0){
+                totalDays = Math.abs(parsedDate1[2] - parsedDate2[2]) - leapYears;
+                totalDays = (totalDays*365) + (leapYears*366);
+            }
+        }
+            
+        else if (parsedDate1[2] == parsedDate2[2]){
+            
+        }
+        
+        else {}
+        
+        return 1;
+    }
+    
+    public static boolean isLeapYear (int year) {
+        if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0))
+            return true;
+        else
+            return false;
+    }
+    
+    
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        String date1 = "1/1/1980";
+        String date2 = "1/1/2022";
+   
+        System.out.println("Entre el " + date1 + " y el " + date2 + " hay " + calculateDays(date1, date2) + " días");
     }
 }
